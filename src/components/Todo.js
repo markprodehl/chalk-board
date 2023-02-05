75f05302542d873de4aca60410e6747d9a347933
markprodehl <markprodehl@gmail.com>
2023-02-04 17:06:30 -0800

    Updated the Tofo component with usestate variables for user, showSignup and showLogin. Also added functions to handle the signup, login andn signout. Then, updated the todoListener to to lok for a user.uid and all of the render components to update based on the authenticated user id

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import firebase from 'firebase/compat/app';
 import 'firebase/compat/database';
+import 'firebase/compat/auth';
 import './Todo.css'
+import SignUp from './SignUp';
+import Login from './Login';
 // This imports the Firebase configuration data from a gitignore file
 import firebaseConfig from '../config/firebaseConfig'
 
@@ -11,6 +14,36 @@ const db = firebase.database().ref("todos");
 function Todo() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [user, setUser] = useState(null);
+  const [showSignUp, setShowSignUp] = useState(false);
+  const [showLogin, setShowLogin] = useState(false);
+
+  const handleSignUp = () => {
+    setShowSignUp(true);
+    setShowLogin(false);
+  };
+
+  const handleLogin = () => {
+    setShowLogin(true);
+    setShowSignUp(false);
+  };
+
+  const handleSignOut = () => {
+    firebase.auth().signOut();
+    setUser(null);
+  };
+
+  useEffect(() => {
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
+
+  
 
   useEffect(() => {
     if ("serviceWorker" in navigator) {
@@ -24,71 +57,85 @@ function Todo() {
       });
     }
 
-    const todoListener = db.on("value", (snapshot) => {
-      const data = snapshot.val();
-      const todos = Object.keys(data || {}).map((key) => ({
-        id: key,
-        ...data[key],
-      }));
-      setTodos(todos);
-    });
-
-    return () => {
-      db.off("value", todoListener);
-    };
-  }, []);
+    if (user) {
+      const todoListener = db.child(user.uid).on("value", (snapshot) => {
+        const data = snapshot.val();
+        const todos = Object.keys(data || {}).map((key) => ({
+          id: key,
+          ...data[key],
+        }));
+        setTodos(todos);
+      });
+      return () => {
+        db.child(user.uid).off("value", todoListener);
+      };
+    }
+  }, [user]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (!newTodo.trim()) return;
-    db.push({ text: newTodo, done: false });
+    if (!newTodo.trim() || !user) return;
+    db.child(user.uid).push({ text: newTodo, done: false });
     setNewTodo("");
   };
 
   const handleToggle = (id) => {
-    db.child(id).update({ done: !todos.find((t) => t.id === id).done });
+    db.child(user.uid).child(id).update({ done: !todos.find((t) => t.id === id).done });
   };
 
   const handleEdit = (id, text) => {
-    db.child(id).update({ text });
+    db.child(user.uid).child(id).update({ text });
   };
 
   const handleDelete = (id) => {
-    db.child(id).remove();
+    db.child(user.uid).child(id).remove();
   };
 
   return (
     <div>
-      <h1 className="header">Chalk Board</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          className="addTodo"
-          type="text"
-          value={newTodo}
-          onChange={(e) => setNewTodo(e.target.value)}
-          placeholder="Add item . . ."
-        />
-        <button type="submit">Add Item</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id}>
+      {user ? (
+        <div>
+          <h1 className="header">Chalk Board</h1>
+          <button onClick={handleSignOut}>Sign Out</button>
+          <form onSubmit={handleSubmit}>
             <input
-              type="checkbox"
-              checked={todo.done}
-              onChange={() => handleToggle(todo.id)}
-            />
-            <input
+              className="addTodo"
               type="text"
-              value={todo.text}
-              onChange={(e) => handleEdit(todo.id, e.target.value)}
+              value={newTodo}
+              onChange={(e) => setNewTodo(e.target.value)}
+              placeholder="Add item . . ."
             />
-            <button onClick={() => handleDelete(todo.id)}>Delete</button>
-          </li>
-        ))}
-      </ul>
+            <button type="submit">Add Item</button>
+          </form>
+          <ul>
+            {todos.map((todo) => (
+              <li key={todo.id}>
+                <input
+                  type="checkbox"
+                  checked={todo.done}
+                  onChange={() => handleToggle(todo.id)}
+                />
+                <input
+                  type="text"
+                  value={todo.text}
+                  onChange={(e) => handleEdit(todo.id, e.target.value)}
+                />
+                <button onClick={() => handleDelete(todo.id)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <div>
+          <button onClick={handleSignUp}>Sign Up</button>
+          <button onClick={handleLogin}>Login</button>
+          {showSignUp && <SignUp />}
+          {showLogin && <Login />}
+        </div>
+      )}
     </div>
   );
+
 }
 
 export default Todo;
